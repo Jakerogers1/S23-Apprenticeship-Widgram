8cc20e1f4d55a610989f6c9d0afcb8265694410b
Jake Rogers <jmanerr@ad.unc.edu>
2023-04-02 19:50:12 -0400

    Initial Commit

---
+++ Widgrams/Widgrams/WidgramsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgramsApp.swift
+//  Widgrams
+//
+//  Created by jmanerr on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct WidgramsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
